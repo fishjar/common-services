d6b0c97b7502b79ca7f00dabb2ad2a08cc30ec81
gabe <yugang2002@gmail.com>
2019-01-02 11:46:19 +0800

    wxuser init

---
+++ wxuser/koa/src/config/index.js
@@ -0,0 +1,25 @@
+import path from 'path';
+import fse from 'fs-extra';
+import { NODE_ENV } from '../lib/env';
+import developmentConfig from './config.development';
+import testConfig from './config.test';
+import productionConfig from './config.production';
+
+const logDir = path.resolve(__dirname, '../log');
+fse.ensureDirSync(logDir);
+
+const defaultConfig = {
+  LOG_PATH: logDir,
+  EXPIRES_IN: '1h',
+};
+
+const configMap = {
+  development: developmentConfig,
+  test: testConfig,
+  production: productionConfig,
+};
+const envConfig = configMap[NODE_ENV];
+
+const config = { ...defaultConfig, ...envConfig };
+
+export default config;
---
+++ wxuser/koa/src/models/Wxuser.js
@@ -0,0 +1,81 @@
+export default (sequelize, DataTypes) => {
+  const Wxuser = sequelize.define(
+    "Wxuser",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV1,
+        primaryKey: true
+      },
+      appid: {
+        type: DataTypes.STRING(32),
+        allowNull: false,
+        unique: false,
+        validate: {}
+      },
+      unionid: {
+        type: DataTypes.STRING(32),
+        allowNull: true,
+        unique: false,
+        validate: {}
+      },
+      openid: {
+        type: DataTypes.STRING(32),
+        allowNull: false,
+        unique: false,
+        validate: {}
+      },
+      sessionKey: {
+        type: DataTypes.STRING(64),
+        allowNull: true,
+        unique: false,
+        validate: {}
+      },
+      nickname: {
+        type: DataTypes.STRING(64),
+        allowNull: true,
+        unique: false,
+        validate: {}
+      },
+      avatar: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+        unique: false,
+        validate: {}
+      },
+      gender: {
+        type: DataTypes.TINYINT,
+        allowNull: true,
+        unique: false,
+        defaultValue: 1,
+        validate: {
+          isIn: [[1, 2, 3]]
+        }
+      },
+      city: {
+        type: DataTypes.STRING(32),
+        allowNull: true,
+        unique: false,
+        validate: {}
+      },
+      province: {
+        type: DataTypes.STRING(32),
+        allowNull: true,
+        unique: false,
+        validate: {}
+      },
+      country: {
+        type: DataTypes.STRING(32),
+        allowNull: true,
+        unique: false,
+        validate: {}
+      }
+    },
+    {
+      underscored: false,
+      tableName: "Wxuser",
+      paranoid: true
+    }
+  )
+  return Wxuser
+}
