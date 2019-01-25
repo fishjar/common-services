7e66891f405894f70389801a95eac0dab954d176
gabe <yugang2002@gmail.com>
2019-01-25 11:14:06 +0800

    fix mq

--- rabbitmq/koa/src/consumer/index.js
+++ rabbitmq/koa/src/consumer/index.js
@@ -19,10 +19,10 @@ import { consumer } from '../lib/mq';
   // Publish/Subscribe
   await consumer(async ch => {
     const ex = 'msgs';
-    await ch.assertExchange(ex, 'fanout', { durable: false });
-    const q = await ch.assertQueue('', { exclusive: true });
-    await ch.bindQueue(q.queue, ex, '');
-    await ch.consume(q.queue, res => {
+    await ch.assertExchange(ex, 'fanout', { durable: false }); // 创建交换器
+    const q = await ch.assertQueue('', { exclusive: true }); // 创建队列
+    await ch.bindQueue(q.queue, ex, ''); // 绑定
+    await ch.consume(q.queue, res => { // 监听消息
       const msg = JSON.parse(res.content.toString());
       console.log('msg', 'fanout', msg);
       // do something...
